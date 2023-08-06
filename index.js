d8336d8b098aaae1b736d93974a65785182d52fb
iconicsequence <icampo@iconicsequence.com>
2023-08-05 20:08:38 -0500

    actualizacion

--- index.js
+++ index.js
@@ -1,74 +1,31 @@
 const express = require('express');
-const https = require('https');
-const { createClient } = require('@supabase/supabase-js');
+const form01Routes = require('./routes/form01');
+const form02Routes = require('./routes/form02');
+const form03Routes = require('./routes/form03');
+const form04Routes = require('./routes/form04');
+const form05Routes = require('./routes/form05');
+const form06Routes = require('./routes/form06');
+const form07Routes = require('./routes/form07');
+const form08Routes = require('./routes/form08');
+const form09Routes = require('./routes/form09');
+const form10Routes = require('./routes/form10');
+const form11Routes = require('./routes/form11');
 
 const app = express();
+
 app.use(express.json({ limit: '50mb' }));  
 
-const supabaseUrl = 'https://fpvxrsabicqnqdswijck.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwdnhyc2FiaWNxbnFkc3dpamNrIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3MTI4Njk2NywiZXhwIjoxOTg2ODYyOTY3fQ.q8Ekn0o6vCkQcUvNIvVK92VlvPxoXXhwXlN0RJb4YZY';
-const supabase = createClient(supabaseUrl, supabaseKey, {
-  persistSession: false
-});
-
-app.post('/upload', async (req, res) => {
-    const bodyData = req.body.data; // Recibe los datos a enviar en la solicitud
-    const fileName = req.body.file_name;
-
-    // Configuración de la solicitud
-    const options = {
-        hostname: 'app.useanvil.com',
-        port: 443,
-        path: '/api/v1/fill/aN0QkXcSWl4GCHI7iVME.pdf',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Basic UWRrWDF6cVNPOEVkaWtnQzNHSmRxMHlsTmc3RTZWQXI6',
-          'Cookie': 'sesh=897784e1a463bcaa47fc71ae02e2482b; sesh.sig=txTRENaY37wogpOStkLDBFCntvE; b_id=1f9fc3360f0b38b5c835433d73d9eeaf; b_id.sig=Em0AnYzHiJx9i7b4VKMnb49Am5c',
-        },
-    };
-
-    // Realiza la solicitud
-    const request = https.request(options, response => {
-        let data = [];
-
-        response.on('data', chunk => {
-            data.push(chunk);
-        });
-
-        response.on('end', async () => {
-            // Concatena los chunks de datos
-            const buffer = Buffer.concat(data);
-
-            // Sube el archivo a Supabase Storage
-            const { error: uploadError } = await supabase.storage.from('pdfs').upload(fileName, buffer);
-
-            if (uploadError) {
-                console.log(uploadError);
-                res.status(500).send("Error al subir el archivo");
-                return;
-            }
-
-            // Genera la URL de descarga
-            const url = await supabase.storage.from('pdfs').getPublicUrl(fileName);
-
-            // Retorna la URL del archivo PDF y el nombre del archivo
-            res.json({
-                link: url,
-                file_name: fileName
-            });
-        });
-    });
-
-    request.on('error', error => {
-        console.log(error);
-        res.status(500).send("Error al hacer la solicitud");
-    });
-
-    // Escribe los datos en el cuerpo de la solicitud
-    request.write(JSON.stringify(bodyData));
-    request.end();
-});
-
+// Usar las rutas definidas en form01
+app.use('/form01', form01Routes);
+app.use('/form02', form02Routes);
+app.use('/form03', form03Routes);
+app.use('/form04', form04Routes);
+app.use('/form05', form05Routes); //SOLICITUD DE BANCAENLINEACORPORATIV Av 021822
+app.use('/form06', form06Routes);
+app.use('/form07', form07Routes);
+app.use('/form08', form08Routes);
+app.use('/form09', form09Routes);
+app.use('/form10', form10Routes); //SOLICITUDDEBANCAENLINEAPERSONA Lv 100919
+app.use('/form11', form11Routes); //Visa Debit Card Form 013 1
 
 app.listen(8080, '0.0.0.0', () => console.log("Server running on port 8080"));
